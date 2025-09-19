49dadf6f1fd35bd0418138b8f2c0961745ac5355
VictorDigitalDealer <victor@digital-dealer.es>
2025-09-19 21:35:13 +0200

    feat[mail]: new configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,11 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: isCI ? [['list'], ['html']] : 'list',
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['json', { outputFile: 'playwright-results.json' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   
   use: {
